05aa5819a552b99cb13b9849bf144f33d15546f5
Konstantin Emonakov <kostya-emonak2002@mail.ru>
2021-11-29 01:04:08 +0300

    updated patterns

--- backend/task/urls.py
+++ backend/task/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('auth/', include('djoser.urls')),
-    path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('login_views/', include('rest_framework.urls')),
     path('', TestTry.as_view())
 ]
